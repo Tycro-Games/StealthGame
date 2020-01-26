e8ebfd2e3d26ee91b940c42b03fcd513c9577c63
OneBogdan01 <31468440+OneBogdan01@users.noreply.github.com>
2020-01-26 19:00:15 +0200

    First commit enemy and player partially ready

---
+++ Game Jam winter first/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityStandardAssets.Characters.ThirdPerson;
+public class PlayerController : MonoBehaviour
+{
+    Camera cam;
+    public LayerMask CanMoveLayer;
+    public float MaxDistance = 100;
+    NavMeshAgent agent;
+    ThirdPersonCharacter character;
+
+    Vector3 DesiredDestination;
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        cam = Camera.main;
+        character = GetComponent<ThirdPersonCharacter>();
+        agent.updateRotation = false;
+    }
+    private void OnDisable()
+    {
+
+        character.StopMovement();
+        agent.enabled = false;
+    }
+    private void OnEnable()
+    {
+        if (agent != null)
+        {
+            agent.enabled = true;
+            agent.destination = DesiredDestination;//resume destination
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, MaxDistance, CanMoveLayer))
+            {
+                agent.SetDestination(hit.point);
+                DesiredDestination = hit.point;
+            }
+        }
+        if (agent.remainingDistance > agent.stoppingDistance)
+        {
+            character.Move(agent.desiredVelocity, false, false);
+        }
+        else
+        {
+            character.Move(Vector3.zero, false, false);
+        }
+    }
+}
---
+++ Game Jam winter first/Assets/Scripts/PlayerShooting.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+    private bool ShouldRotate = false;
+    Camera cam;
+    PlayerController playerController;
+    [Header("Shooting settings")]
+    public LayerMask CanAtackLayer;
+    public float MaxDistance = 100;
+    public float fireRate = 1f;
+    private float lastFirerate = 0;
+    private float TimeBetweenShots;
+    public float smoothRotation = .25f;
+    private Vector3 Point;
+    private void Start()
+    {
+        playerController = GetComponent<PlayerController>();
+        cam = Camera.main;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            playerController.enabled = false;
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, MaxDistance, CanAtackLayer))
+            {
+                FireRateCheck(hit.point);
+            }
+        }
+        if (ShouldRotate)
+        {
+            Rotate(Point);
+        }
+    }
+    void FireRateCheck(Vector3 point)
+    {
+        if (Time.time > lastFirerate)
+        {
+            Point = point;
+            lastFirerate = Time.time + fireRate / 2;
+            TimeBetweenShots = lastFirerate - Time.time;
+            ShouldRotate = true;//rotate to target
+            StartCoroutine(Atack());
+        }
+
+
+    }
+    IEnumerator Atack()
+    {
+        yield return new WaitForSeconds(TimeBetweenShots);
+        //shooting and shit
+        ShouldRotate = false;
+        playerController.enabled = true;
+    }
+    void Rotate(Vector3 target)
+    {
+        Vector3 offset = target - transform.position;
+        Quaternion desiredRot = Quaternion.LookRotation(offset);
+
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, desiredRot, smoothRotation);
+    }
+}
